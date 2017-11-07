bb7457c03a8bbd4d212ade7729080561b1a91ce9
Andrew Hall <hndrewaall@gmail.com>
2017-11-07 16:31:59 -0500

    Add Flower monitor

--- app/autoapp.py
+++ app/autoapp.py
@@ -3,8 +3,10 @@
 from flask.helpers import get_debug_flag
 
 from league.app import create_app
+from league.celery import create_celery
 from league.settings import DevConfig, ProdConfig
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
+celery = create_celery(app)
