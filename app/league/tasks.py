e219e29a385418e80d0ef0b53b21c7d30ae73909
Andrew Hall <hndrewaall@gmail.com>
2017-11-09 20:13:34 -0500

    Add AGA sync task

--- app/league/tasks.py
+++ app/league/tasks.py
@@ -1,9 +1,35 @@
 # -*- coding: utf-8 -*-
 """Celery tasks."""
+import math
+
+from bs4 import BeautifulSoup
 from celery import current_app
+from requests import get
+
+from league.models import Player
 
 
 @current_app.task
 def hello_world():
     """Hello world task."""
     return 'Hello world!'
+
+
+@current_app.task
+def sync_aga_data():
+    """Update all local AGA data by scraping the AGA servers."""
+    for player in Player.get_players():
+        url = 'http://www.usgo.org/ratings-lookup-id?PlayerID={}'.format(
+            player.aga_id)
+        data = get(url)
+        soup = BeautifulSoup(data.text, 'html.parser')
+        columns = [col.text for col in soup.findAll('tr')[1].findAll('td')]
+        if int(columns[0]) == player.aga_id:
+            rating = float(columns[2])
+            if rating > 0:
+                rank = math.floor(rating)
+            else:
+                rank = math.ceil(rating)
+            player.update(aga_rank=rank)
+
+    return
