a284cd7ff9a33c8b9922e21c23c3d877480e326d
Andrew Hall <hndrewaall@gmail.com>
2017-11-09 21:24:29 -0500

    Docker dev config DON'T MERGE ME

--- app/league/settings.py
+++ app/league/settings.py
@@ -55,10 +55,16 @@ class DevConfig(Config):
 
     ENV = 'dev'
     DEBUG = True
-    DB_NAME = 'dev.db'
-    # Put the db file in project root
-    DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
+    POSTGRES_DB = os.environ.get('POSTGRES_DB', 'postgresql')
+    POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgresql')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', '')
+    POSTGRES_ADDRESS = os.environ.get('POSTGRES_ADDRESS', 'db')
+    SQLALCHEMY_DATABASE_URI = ('postgresql://{}:{}@{}/{}').format(
+        POSTGRES_USER,
+        POSTGRES_PASSWORD,
+        POSTGRES_ADDRESS,
+        POSTGRES_DB
+    )
     DEBUG_TB_ENABLED = True
     ASSETS_DEBUG = True  # Don't bundle/minify static assets
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
---
+++ app/migrate_and_run_dev.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+export FLASK_APP=autoapp.py
+
+while true
+do
+    flask db upgrade && break
+    sleep 1
+done
+
+uwsgi --chmod-socket=666 -s /tmp/uwsgi/uwsgi.sock --plugin python3 --manage-script-name --mount /=autoapp:flaskapp --python-autoreload=1
